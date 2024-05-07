d21c0bd7e31f5dcacab75f8a50bac02a336d2818
Dhanang Pratama <dhanangarfian@gmail.com>
2024-05-08 00:41:09 +0700

    refactor: change key to use string

--- src/Status.php
+++ src/Status.php
@@ -4,12 +4,28 @@ namespace Spatie\ModelStatus;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Support\Str;
 
 class Status extends Model
 {
     protected $guarded = [];
 
     protected $table = 'statuses';
+
+    protected static function boot() {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->id = (string)Str::orderedUuid();
+        });
+    }
+
+    public function getIncrementing(): bool {
+        return false;
+    }
+
+    public function getKeyType(): string {
+        return 'string';
+    }
 
     public function model(): MorphTo
     {
